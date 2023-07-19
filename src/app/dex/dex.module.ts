33fae1a9398e9c56c0b7f3860bc6061486104ed5
Eduardo Rambauske <dudurpp1@gmail.com>
2023-07-19 15:12:26 -0300

    Customized types in list card and started customizing search bar

--- src/app/dex/dex.module.ts
+++ src/app/dex/dex.module.ts
@@ -5,12 +5,14 @@ import { DexComponent } from './components/dex/dex.component';
 import { MaterialModule } from '../material/material.module';
 import { CardComponent } from './components/card/card.component';
 import { ListComponent } from './components/list/list.component';
+import { SearchComponent } from './components/search/search.component';
+import { FormsModule } from '@angular/forms';
 
 const routes : Routes = [{path: '', component: DexComponent}];
 
 @NgModule({
-  declarations: [DexComponent, CardComponent, ListComponent],
-  imports: [CommonModule, RouterModule.forChild(routes),MaterialModule],
+  declarations: [DexComponent, CardComponent, ListComponent, SearchComponent],
+  imports: [FormsModule,CommonModule, RouterModule.forChild(routes),MaterialModule],
   providers: [],
 })
 export class DexModule {}
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,12 +3,18 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
 
 const materialComponents = [
     MatSlideToggleModule,
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
 ]
 
 @NgModule({
